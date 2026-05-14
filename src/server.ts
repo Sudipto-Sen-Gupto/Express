2c287eb388dfe9eaaa83635a251e0f74d49a21da
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-14 20:10:02 +0600

    create a path for server and cloud database communication and usage of middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { type Application, type Request, type Response } from "express"
+import  { Pool } from "pg"
 const app :Application = express()
 const port = 3000
-app.use(express.json())
+app.use(express.json()) //middleware for json data
+app.use(express.urlencoded({extended:true}))
+app.use(express.text())
 app.get('/', (req : Request, res:Response) => {
   res.status(200).json({
     "Message":"Api worked properly"
@@ -10,8 +13,19 @@ app.get('/', (req : Request, res:Response) => {
   })
 })
 
+  const pool=new Pool({
+    connectionString:"postgresql://neondb_owner:npg_hnOyUv9a4REq@ep-long-firefly-aqzfrzdf-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  })
+
 app.post('/',async(req:Request,res:Response)=>{
-    console.log(req.body);
+    const {name,email,password} =req.body;
+    // res.send(body)
+    res.status(201).json({
+        message:"Created post",
+         data:{
+            name,email
+         }
+    })
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
