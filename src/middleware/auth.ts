2605b082020f0cf7fca50b9f70351e945275b2d2
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-20 11:27:10 +0600

    decode token from encode by using jwt.verify() method and implement some validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express"
-
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import { pool } from "../dbNeon";
  const auth=()=>{
      return async(req:Request,res:Response,next:NextFunction)=>{
               console.log(req.headers);
@@ -11,6 +12,30 @@ import type { NextFunction, Request, Response } from "express"
                          message:"Unauthorized access"
                     })
               }
+            
+              const decodedToken=await jwt.verify(token as string ,process.env.SECTRET_KEY as string) as JwtPayload;
+              console.log(decodedToken);  //decoded token from encoded token
+
+              const validUser=await pool.query(`
+                  SELECT * FROM userDB WHERE email=$1  
+                `,[decodedToken.email]) //decodedToken send as payload then we get email
+               
+                
+
+                const user=validUser.rows[0]
+
+                if(user.length===0){
+                     res.status(403).json({
+                        message:"Forbidden user, no user available"
+                     })
+                }
+
+                if(user.age<28){
+                     res.status(403).json({
+                          message:"Forbidden for this user"
+                     })
+                }
+
               next()
      }
  }
